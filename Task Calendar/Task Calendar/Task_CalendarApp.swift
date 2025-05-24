50dca3b7a33ce08fa079c6087cda42c27c622a17
Wilson Zhang <z5587836@ad.unsw.edu.au>
2025-05-24 18:38:57 +1000

    Basis made on XCode

---
+++ Task Calendar/Task Calendar/Task_CalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_CalendarApp.swift
+//  Task Calendar
+//
+//  Created by Wilson Zhang on 24/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct Task_CalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Task Calendar/Task CalendarTests/Task_CalendarTests.swift
@@ -0,0 +1,17 @@
+//
+//  Task_CalendarTests.swift
+//  Task CalendarTests
+//
+//  Created by Wilson Zhang on 24/5/2025.
+//
+
+import Testing
+@testable import Task_Calendar
+
+struct Task_CalendarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
